aa57ea96e7483dd0cb45615f4e0ce9a1485098bc
UnrelatedString <unrelatedstring@gmail.com>
2025-06-08 19:49:08 -0400

    i am this close to MASSSSSIVELY overengineering the mere CONCEPT OF A RESULT

--- src/Test/FlexLaws/Core.purs
+++ src/Test/FlexLaws/Core.purs
@@ -10,10 +10,9 @@ import Prelude
 import Test.QuickCheck
   ( class Arbitrary
   , class Coarbitrary
-  , class Testable
+  , Result
   , Gen
   )
-import Type.Row (type (+))
 
 -- | A suite of tests corresponding to the laws of a class,
 -- | for a given type.
@@ -30,11 +29,13 @@ newtype LawTest :: forall k. Row Type -> k -> Type
 newtype LawTest r a = LawTest
   { lawName :: String
   , lawDescription :: LawDescription
-  , test :: forall b. (forall p. Testable p => p -> b) -> Record r -> b
+  , test :: Record r -> Gen Result
   }
 
 type NeedsArbitrary a r = ( arbitrary :: Gen a | r )
 type NeedsCoarbitrary a r = ( coarbitrary :: forall b. a -> Gen b -> Gen b | r )
+type NeedsArbitrary1 f r = ( arbitrary :: forall a. Arbitrary a => Gen (f a) | r )
+type NeedsCoarbitrary1 f r = ( coarbitrary :: forall a b. Coarbitrary a => f a -> Gen b -> Gen b | r )
 
 -- | The description of a law, either
 -- | as plain text to reproduce faithfully,
---
+++ src/Test/FlexLaws/Prelude.purs
@@ -0,0 +1,7 @@
+-- | Re-exports expected to be consistently needed in defining law tests.
+
+module Test.FlexLaws.Prelude (module Reexports) where
+
+import Prelude as Reexports
+-- will add core stuff when that;s like actually stable lmao
+import Type.Row (type (+)) as Reexports
